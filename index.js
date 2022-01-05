4193d0005f483b3a01379a8215ff7dec3bdce9ca
manishboro <officialmanishboro@gmail.com>
2022-01-05 13:02:30 +0530

    added html

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 import express from "express";
-const app = express();
-
-// A module for serving http and https connections over the same port.
-import https from "httpolyglot";
 import fs from "fs";
 import path from "path";
-const __dirname = path.resolve();
+import https from "httpolyglot"; // A module for serving http and https connections over the same port.
 
-app.get("/", (req, res) => {
-  res.send("Hello from mediasoup app!");
-});
+const app = express();
+const __dirname = path.resolve();
 
 const options = {
   key: fs.readFileSync("./server/ssl/key.pem", "utf-8"),
   cert: fs.readFileSync("./server/ssl/cert.pem", "utf-8"),
 };
 
-const httpsServer = https.createServer(options, app);
+app.get("/", (req, res) => res.send("Hello from mediasoup app!"));
 
-httpsServer.listen(3055, () => {
-  console.log("listening on port: " + 3055);
-});
+app.use("/sfu", express.static(path.join(__dirname, "public")));
+
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(3055, () => console.log("listening on port: " + 3055));
